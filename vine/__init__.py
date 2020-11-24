5eccd7fefb98fe1da33ad6fd1b76c711dea5185c
Matt Hanger <matt@matthanger.net>
2020-11-24 16:07:12 -0500

    ...

--- vine/__init__.py
+++ vine/__init__.py
@@ -14,7 +14,7 @@ from .funtools import (
 )
 
 
-__version__ = '1.3.0.1'
+__version__ = '1.3.0'
 __author__ = 'Ask Solem'
 __contact__ = 'ask@celeryproject.org'
 __homepage__ = 'http://github.com/celery/vine'
